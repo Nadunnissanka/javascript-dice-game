fbac1aed96c6d8d3e5f2d5d3bcbb9ca2b8729ba7
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2020-12-26 19:33:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,35 @@
-function diceeOne(){
-    var randomNumber1 = Math.floor((Math.random()*6)+1);
-    // console.log(randomNumber1);
-    switch(randomNumber1)
-    {
-        case 1:
-            document.querySelector(".dice-img-1").setAttribute("src","img/dice1.png");
-            return randomNumber1;
-            break;
-        case 2:
-            document.querySelector(".dice-img-1").setAttribute("src","img/dice2.png");
-            return randomNumber1;
-            break;
-        case 3:
-            document.querySelector(".dice-img-1").setAttribute("src","img/dice3.png");
-            return randomNumber1;
-            break;
-        case 4:
-            document.querySelector(".dice-img-1").setAttribute("src","img/dice4.png");
-            return randomNumber1;
-            break;
-        case 5:
-            document.querySelector(".dice-img-1").setAttribute("src","img/dice5.png");
-            return randomNumber1;
-            break;
-        case 6:
-            document.querySelector(".dice-img-1").setAttribute("src","img/dice6.png");
-            return randomNumber1;
-            break;
-        default:
-            alert("Code is invalid! Please Check the code!!");
-            break;
-    }
-    return randomNumber1;
+function randomOne(){
+    var randomnumber1 = Math.floor((Math.random() * 6) + 1);
+
+    var randomImageNameOne = "img/dice"+ randomnumber1 +".png";
+
+    document.querySelector(".dice-img-1").setAttribute("src",randomImageNameOne);
+
+    return randomnumber1;
 }
 
-function diceeTwo(){
-    var randomNumber2 = Math.floor((Math.random()*6)+1);
-    //console.log(randomNumber2);
-    switch(randomNumber2)
-    {
-        case 1:
-            document.querySelector(".dice-img-2").setAttribute("src","img/dice1.png");
-            return randomNumber2;
-            break;
-        case 2:
-            document.querySelector(".dice-img-2").setAttribute("src","img/dice2.png");
-            return randomNumber2;
-            break;
-        case 3:
-            document.querySelector(".dice-img-2").setAttribute("src","img/dice3.png");
-            return randomNumber2;
-            break;
-        case 4:
-            document.querySelector(".dice-img-2").setAttribute("src","img/dice4.png");
-            return randomNumber2;
-            break;
-        case 5:
-            document.querySelector(".dice-img-2").setAttribute("src","img/dice5.png");
-            return randomNumber2;
-            break;
-        case 6:
-            document.querySelector(".dice-img-2").setAttribute("src","img/dice6.png");
-            return randomNumber2;
-            break;
-        default:
-            alert("Code is invalid! Please Check the code!!");
-            break;
-    }
+function randomTwo(){
+    var randomnumber2 = Math.floor((Math.random() * 6) + 1);
+
+    var randomImageNameTwo = "img/dice"+ randomnumber2 +".png";
+
+    document.querySelector(".dice-img-2").setAttribute("src",randomImageNameTwo);
+
+    return randomnumber2;
 }
 
 function main(){
-    diceeOne();
-    diceeTwo();
-    if(diceeOne() > diceeTwo()){
-        console.log("player one win");
-        document.querySelector("#winner").innerText = "Player One";
+    randomOne();
+    randomTwo();
+    var a = randomOne();
+    var b = randomTwo();
+    //console.log(a);
+    //console.log(b);
+    if(a > b){
+        document.querySelector("#winner").innerHTML="Player One Win!";
+    }else if(b > a){
+        document.querySelector("#winner").innerHTML="Player Two Win!";
     }else{
-        console.log("player two win");
-        document.querySelector("#winner").innerText = "Player Two";
+        document.querySelector("#winner").innerHTML="Draw"; 
     }
-
 }
